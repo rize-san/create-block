9fce11ac7427f2230632bb91c57b4e414f6335b1
rize <114392935+rize-san@users.noreply.github.com>
2022-11-21 16:32:29 +0200

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function creatBlock(){
-let box = document.querySelector('.box')
-let newBlock = document.createElement('div')
-newBlock.classList = ('mini')
-box.append(newBlock)                                 
-}
